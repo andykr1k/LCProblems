cd4ec7c6a5c25023bbf2f8b7efca614867b4e575
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-06-21 13:36:16 -0700

    Adding First and Last Element Brute Force

---
+++ firstandlastelement.cpp
@@ -0,0 +1,34 @@
+///PROBLEM #34
+
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> result;
+        vector<int> finished;
+        vector<int> fals = {-1,-1};
+        bool res = false;
+        for (int i = 0; i < nums.size(); i++){
+            if (target == nums[i]){
+                result.push_back(i);
+                res = true;
+            }
+        }
+        if (result.size()==1){
+            result.push_back(result[0]);
+        }
+        if (result.size()>1){
+            finished.push_back(result[0]);
+            finished.push_back(result[result.size()-1]);
+        }
+        if (res){
+            return finished;
+        } else {
+            return fals;
+        }
+        
+    }
+};
