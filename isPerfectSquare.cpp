5734c4c5def13ae43b64b5102ce91fa226aa5f4e
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-06-21 15:20:00 -0700

    Adding isPerfectSquares and peakIndexMountain

---
+++ isPerfectSquare.cpp
@@ -0,0 +1,15 @@
+///PROBLEM #367
+
+
+//BRUTE FORCE
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        for (int i = 1; i < 46341; i++){
+            if (i * i == num){
+                return true;
+            }
+        }
+        return false;
+    }
+};
---
+++ peakIndexOfMountain.cpp
@@ -0,0 +1,22 @@
+///PROBLEM #852
+
+#include <vector>
+using namespace std;
+
+
+//BRUTE FORCE
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        for (int i = 0; i < arr.size(); i++){
+            
+            if (i != 0 && i != arr.size()-1){
+                if (arr[i-1] < arr[i] && arr[i] > arr[i + 1]){
+                    return i;
+                }
+            }
+            
+        }
+        return 0;
+    }
+};
