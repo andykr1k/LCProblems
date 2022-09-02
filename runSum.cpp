7a0cb24c70901eeca2285f288e969e73d042a34c
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-09-02 14:05:30 -0700

    Adding New Problems

---
+++ runSum.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> run;
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++){
+            sum = sum + nums[i];
+            run.push_back(sum);
+        }
+        return run;
+    }
+};
